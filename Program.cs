72ad1e368ffe1ab02811632cc5cec72b80fee028
abc <abc@gmail.com>
2024-02-16 09:27:54 +0530

    All Files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*** Even Number ***");
+NumberSystem.EvenNum();
+Console.WriteLine("*** Odd Number ***");
+NumberSystem.OddNum();
